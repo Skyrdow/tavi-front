682e8ad1f120d3be63954d27fae50ac8a90b5600
Lucas Mesias <lucas.mesias@usach.cl>
2025-11-05 14:49:05 -0300

    add: maqueta

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,14 @@
 <template>
-  <nav class="flex items-center p-2 gap-2">
-    <img src="/favicon.ico" />
-    <h1>Easy CManager</h1>
-    <span>login</span>
-    <span>botones</span>
-    <span>botones</span>
-    <span>botones</span>
+  <nav class="flex items-center p-2 justify-evenly">
+    <span class="flex items-center gap-2">
+      <img src="/favicon.ico" />
+      <h1 class="">Easy CManager</h1>
+    </span>
+    <span class="flex items-center gap-4">
+      <NuxtLink class="hover:underline" href="/content">Contenido</NuxtLink>
+      <NuxtLink class="hover:underline" href="/programing">Programación</NuxtLink>
+      <NuxtLink class="hover:underline" href="/mod">Moderación</NuxtLink>
+    </span>
   </nav>
   <NuxtPage />
 </template>
---
+++ app/pages/content.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="grid grid-cols-2 m-4 p-1 bg-gray-300">
+    <div class="bg-green-100">chat</div>
+    <div class="w-md bg-white">
+      <span class="flex items-center gap-2 m-1">
+        <img class="size-12" src="/assets/account.svg" />
+        <p>nombre.negocio</p>
+      </span>
+      <div class="aspect-square w-full bg-gray-400"></div>
+      <span class="flex justify-between w-full">
+        <span class="inline-flex">
+          <img class="size-10" src="/assets/like.svg" />
+          <img class="size-10" src="/assets/comment.svg" />
+          <img class="size-10" src="/assets/send.svg" />
+        </span>
+        <img class="size-10" src="/assets/save.svg" />
+      </span>
+      <p class="mx-2">
+        <b>nombre.negocio</b> descripcion IA: Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Ut semper metus in mi efficitur, in rutrum
+        sapien luctus. Vestibulum egestas felis venenatis gravida blandit.
+        Nullam fermentum gravida auctor. Etiam pretium egestas elit, congue
+        mollis lacus iaculis id. Aenean vulputate blandit egestas. Maecenas
+        pharetra porta tristique. Sed enim tellus, euismod eu nisi at, ultricies
+        fringilla eros. Praesent mi leo, vehicula volutpat est id, elementum
+        cursus magna. Etiam at porttitor tortor, sed tincidunt dolor.       </p>
+    </div>
+  </div>
+</template>
