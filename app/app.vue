8c0e10684632cae361d1564acfc8baea2a75a7c0
Lucas Mesias <lucas.mesias@usach.cl>
2025-11-04 22:05:53 -0300

    add: nuxt init

---
+++ app/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav class="flex items-center p-2 gap-2">
+    <img src="/favicon.ico" />
+    <h1>Easy CManager</h1>
+    <span>login</span>
+    <span>botones</span>
+    <span>botones</span>
+    <span>botones</span>
+  </nav>
+  <NuxtPage />
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  css: ['./app/assets/main.css'],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxt/test-utils",
+    "@nuxt/ui",
+  ],
+});
